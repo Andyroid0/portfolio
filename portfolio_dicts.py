9b8a5fd34e705b5c1ae0183070d7c154a4d9fca9
Andrew Valley <andyvalley@live.com>
2020-10-11 12:32:10 -0400

    dicts fix

---
+++ portfolio_dicts.py
@@ -0,0 +1,129 @@
+#! python3
+
+Creatures_cards = [
+
+    {
+        'img_NAME': 'slugger_polish.png',
+        'DESC': 'Slug creature design brought to a decent level of finish.'
+    },
+
+    {
+        'img_NAME': 'krok.png',
+        'DESC': 'Crocodile creature design.'
+    },
+
+    {
+        'img_NAME': 'sharoom.png',
+        'DESC': 'Creature design for a 2D turnbased battle game.'
+    },
+
+    {
+        'img_NAME': 'kraken_me_up.png',
+        'DESC': 'Older character design from a drawing prompt on instagram.'
+    },
+
+    {
+        'img_NAME': 'Rubberducky@0.5x.png',
+        'DESC': 'Rough painting in Adobe Fresco, done for proko instagram drawing prompt.'
+    },
+
+    {
+        'img_NAME': 'wisper (1)@0.5x.png',
+        'DESC': 'Creature design for a 2D turnbased combat game.'
+    }
+
+]
+
+Characters_cards = [
+
+    {
+        'img_NAME': 'witchy.png',
+        'DESC': 'Witch character design for Instagram Drawlloween'
+    }
+
+]
+
+
+
+
+Sketchbook_cards = [
+    {
+        'img_NAME': 'color_scene.png',
+        'DESC': 'Loose pleinaire sketch in Adobe Fresco.'
+    },
+
+    {
+        'img_NAME': 'stillLife_value1.png',
+        'DESC': 'Diffused lighting Study'
+    }
+
+    
+]
+
+Environment_cards = []
+
+Motion_cards = []
+
+Props_cards = [
+    {
+        'img_NAME': 'drawing_prompt.png',
+        'DESC': 'Kids Lunch drawing prompt, done for an application.'
+    }
+]
+
+Animation_cards = []
+
+General_cards = [
+
+    {
+        'img_NAME': 'witchy.png',
+        'DESC': 'Witch character design for Instagram Drawlloween'
+    },
+
+    {
+        'img_NAME': 'slugger_polish.png',
+        'DESC': 'Slug creature design brought to a decent level of finish.'
+    },
+
+    {
+        'img_NAME': 'sharoom.png',
+        'DESC': 'Creature design for a 2D turnbased battle game.'
+    },
+
+    {
+        'img_NAME': 'drawing_prompt.png',
+        'DESC': 'Kids Lunch drawing prompt, done for an application.'
+    },
+
+    {
+        'img_NAME': 'color_scene.png',
+        'DESC': 'Loose pleinaire sketch in Adobe Fresco.'
+    },
+
+    {
+        'img_NAME': 'krok.png',
+        'DESC': 'Crocodile creature design.'
+    },
+
+    {
+        'img_NAME': 'kraken_me_up.png',
+        'DESC': 'Older character design from a drawing prompt on instagram.'
+    },
+
+    {
+        'img_NAME': 'Rubberducky@0.5x.png',
+        'DESC': 'Rough painting in Adobe Fresco, done for proko instagram drawing prompt.'
+    },
+
+    {
+        'img_NAME': 'wisper (1)@0.5x.png',
+        'DESC': 'Creature design for a 2D turnbased combat game.'
+    },
+
+    {
+        'img_NAME': 'stillLife_value1.png',
+        'DESC': 'Diffused lighting Study'
+    }
+
+]
+
